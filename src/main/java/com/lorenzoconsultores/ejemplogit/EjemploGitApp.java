85507c85f19abc9e46712ddabd05939d2622718c
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2022-09-06 19:34:46 +0100

    Initial variable and method

--- src/main/java/com/lorenzoconsultores/ejemplogit/EjemploGitApp.java
+++
@@ -1,6 +0,0 @@
-package com.lorenzoconsultores.ejemplogit;
-public class EjemploGitApp {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-    }
-}
---
+++ src/main/java/com/lorenzoconsultores/fizzbuzzkata/FizzBuzzKata.java
@@ -0,0 +1,21 @@
+package com.lorenzoconsultores.fizzbuzzkata;
+
+public class FizzBuzzKata {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++){
+
+
+
+        }
+
+    }
+}
+
+class PreparationFizzBuzzKata{
+
+    public String getResult(int i){return null;}
+
+
+
+}
